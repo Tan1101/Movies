cb0af42f1c78c5f756196710d76225038076b5b9
tan <tannqhe161338@fpt.edu.vn>
2023-08-08 20:43:57 +0700

    add Project

---
+++ Movies-PRN211/Movies-PRN211/Controllers/RegisterController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies_PRN211.Interfaces;
+using Movies_PRN211.Models;
+
+namespace Movies_PRN211.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public RegisterController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public IActionResult Register()
+        {
+            if (TempData["errorExist"] != null)
+            {
+                ViewBag.exit = TempData["errorExist"];
+            }
+            return View(new Account());
+        }
+
+        [HttpPost]
+        public IActionResult Register(Account model)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkExit = _accountRepository.isExits(model.Gmail);
+                if (checkExit)
+                {
+                    TempData["errorExist"] = "User Name Is Exist";
+                    ViewBag.exit = TempData["errorExist"];
+                    return View(model);
+                }
+                else
+                {
+                    _accountRepository.AddAccount(model);
+                    TempData["success"] = "Your account has just been created successfully";
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+    }
+}
